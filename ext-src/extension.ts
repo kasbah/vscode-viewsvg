407edefd4db90c63558e3c07f325fe105461ea80
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-03-05 19:10:34 +0000

    Rename title to View SVG

--- ext-src/extension.ts
+++ ext-src/extension.ts
@@ -36,7 +36,7 @@ class ReactPanel {
 		this._extensionPath = extensionPath;
 
 		// Create and show a new webview panel
-		this._panel = vscode.window.createWebviewPanel(ReactPanel.viewType, "React", column, {
+		this._panel = vscode.window.createWebviewPanel(ReactPanel.viewType, "View SVG", column, {
 			// Enable javascript in the webview
 			enableScripts: true,
 
@@ -102,7 +102,7 @@ class ReactPanel {
 				<meta charset="utf-8">
 				<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
 				<meta name="theme-color" content="#000000">
-				<title>React App</title>
+				<title>View SVG</title>
 				<link rel="stylesheet" type="text/css" href="${styleUri}">
 				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
 				<base href="${vscode.Uri.file(path.join(this._extensionPath, 'build')).with({ scheme: 'vscode-resource' })}/">
